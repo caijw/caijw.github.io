2451277dd8afb5bc47144ccb400678ada4a8f42b
kingweicai(蔡经纬) <kingweicai@tencent.com>
2018-12-31 22:54:29 +0800

    feat: move

--- asserts/js/crop.js
+++ asserts/js/crop.js
@@ -1,141 +1,217 @@
 function colorForTouch(touch) {
-  var r = touch.identifier % 16;
-  var g = Math.floor(touch.identifier / 3) % 16;
-  var b = Math.floor(touch.identifier / 7) % 16;
-  r = r.toString(16); // make it a hex digit
-  g = g.toString(16); // make it a hex digit
-  b = b.toString(16); // make it a hex digit
-  var color = "#" + r + g + b;
-  console.log("color for touch with identifier " + touch.identifier + " = " + color);
-  return color;
+    var r = touch.identifier % 16;
+    var g = Math.floor(touch.identifier / 3) % 16;
+    var b = Math.floor(touch.identifier / 7) % 16;
+    r = r.toString(16); // make it a hex digit
+    g = g.toString(16); // make it a hex digit
+    b = b.toString(16); // make it a hex digit
+    var color = "#" + r + g + b;
+    console.log("color for touch with identifier " + touch.identifier + " = " + color);
+    return color;
 }
 
-function copyTouch(touch) {
-  return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
-}
 
-function ongoingTouchIndexById(idToFind) {
-  for (var i = 0; i < ongoingTouches.length; i++) {
-    var id = ongoingTouches[i].identifier;
-    
-    if (id == idToFind) {
-      return i;
-    }
-  }
-  return -1;    // not found
-}
 
 function log(msg) {
-	console.log(msg);
+    console.log(msg);
 }
-var ongoingTouches = [];
 
-window.Cropper = function Cropper(opts){
-	opts = opts || {};
-	this.canvas = opts.canvas;
-	this.imgUrl = opts.imgUrl;
-	this.imgWidth = opts.imgWidth;
-	this.imgHeight = opts.imgHeight;
-	this.ongoingTouches = [];
-	this.canvas._cropper = this;
-
-	this.canvas.addEventListener("touchstart", this.handleStart, false);
-	this.canvas.addEventListener("touchmove", this.handleMove, false);
-	this.canvas.addEventListener("touchend", this.handleEnd, false);
-	this.canvas.addEventListener("touchcancel", this.handleCancel, false);
-
+window.Cropper = function Cropper(opts) {
+	var that = this;
+    opts = opts || {};
+    that.canvas = opts.canvas || null;
+    that.imgUrl = opts.imgUrl || null;
 };
 
-Cropper.prototype.handleStart = function (evt) {
-	var cropper = this._cropper;
-	evt.preventDefault();
-	console.log("touchstart.");
-	var el = document.getElementsByTagName("canvas")[0];
-	var ctx = el.getContext("2d");
-	var touches = evt.changedTouches;
-	      
-	for (var i = 0; i < touches.length; i++) {
-	  console.log("touchstart:" + i + "...");
-	  ongoingTouches.push(copyTouch(touches[i]));
-	  var color = colorForTouch(touches[i]);
-	  ctx.beginPath();
-	  ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-	  ctx.fillStyle = color;
-	  ctx.fill();
-	  console.log("touchstart:" + i + ".");
-	}
+Cropper.prototype.init = function (opts) {
+	var that = this;
+	return new Promise(function (resolve, reject) {
+		opts = opts || {};
+		that.canvas = opts.canvas || that.canvas || null;
+		that.imgUrl = opts.imgUrl || that.imgUrl || null;
+		that.canvasWidth = that.canvas.width || 0;
+		that.canvasHeight = that.canvas.height || 0;
+		that.ongoingTouches = [];
+		that.originX = 0;
+		that.originY = 0;
+		that.curOriginX = 0;
+		that.curOriginY = 0;
+
+		if(!that.canvas){
+			reject('no canvas');
+			return;
+		}
+		var img = new Image(); 	
+		img.onload = function(){
+			that.canvas._cropper = that;
+			that.img = img;
+			that.imgWidth = img.width;
+			that.imgHeight = img.height;
+
+			that.canvas.addEventListener("touchstart", that.handleStart, false);
+			that.canvas.addEventListener("touchmove", that.handleMove, false);
+			that.canvas.addEventListener("touchend", that.handleEnd, false);
+			that.canvas.addEventListener("touchcancel", that.handleCancel, false);
+
+			var ctx = that.canvas.getContext('2d');
+			ctx.drawImage(
+				that.img,
+				0, 0,
+				that.imgWidth, that.imgHeight,
+				0, 0,
+				that.imgWidth, that.imgHeight
+			);
+
+			resolve();
+		};
+		img.onerror = function(err){
+			reject(err);
+		};
+		img.src = that.imgUrl;
+	});
 };
 
-Cropper.prototype.handleMove = function (evt) {
-	var cropper = this._cropper;
-	evt.preventDefault();
-	var el = document.getElementsByTagName("canvas")[0];
-	var ctx = el.getContext("2d");
-	var touches = evt.changedTouches;
-
-	for (var i = 0; i < touches.length; i++) {
-	  var color = colorForTouch(touches[i]);
-	  var idx = ongoingTouchIndexById(touches[i].identifier);
-
-	  if (idx >= 0) {
-	    console.log("continuing touch "+idx);
-	    ctx.beginPath();
-	    console.log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
-	    ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-	    console.log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-	    ctx.lineTo(touches[i].pageX, touches[i].pageY);
-	    ctx.lineWidth = 4;
-	    ctx.strokeStyle = color;
-	    ctx.stroke();
-
-	    ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-	    console.log(".");
-	  } else {
-	    console.log("can't figure out which touch to continue");
-	  }
-	}
+/*
+	clear
+*/
+Cropper.prototype.uninit = function () {
+	var that = this;
+	that.canvas.removeEventListener("touchcancel", that.handleCancel);
+	that.canvas.removeEventListener("touchend", that.handleEnd);
+	that.canvas.removeEventListener("touchmove", that.handleMove);
+	that.canvas.removeEventListener("touchstart", that.handleStart);
+	that.imgHeight = 0;
+	that.imgWidth = 0;
+	that.img = null;
+	that.canvas._cropper = null;
+	that.ongoingTouches = [];
+	that.imgUrl = '';
+	that.canvas = null;
 };
 
-Cropper.prototype.touchend = function (evt) {
-	var cropper = this._cropper;
-	evt.preventDefault();
-	log("touchend");
-	var el = document.getElementsByTagName("canvas")[0];
-	var ctx = el.getContext("2d");
-	var touches = evt.changedTouches;
+Cropper.prototype.copyTouch = function(touch) {
+    return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+}
 
-	for (var i = 0; i < touches.length; i++) {
-	  var color = colorForTouch(touches[i]);
-	  var idx = ongoingTouchIndexById(touches[i].identifier);
+Cropper.prototype.ongoingTouchIndexById = function(idToFind) {
+    for (var i = 0; i < this.ongoingTouches.length; i++) {
+        var id = this.ongoingTouches[i].identifier;
 
-	  if (idx >= 0) {
-	    ctx.lineWidth = 4;
-	    ctx.fillStyle = color;
-	    ctx.beginPath();
-	    ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-	    ctx.lineTo(touches[i].pageX, touches[i].pageY);
-	    ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
-	    ongoingTouches.splice(idx, 1);  // remove it; we're done
-	  } else {
-	    console.log("can't figure out which touch to end");
-	  }
-	}
+        if (id == idToFind) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+Cropper.prototype.handleStart = function(evt) {
+
+    evt.preventDefault();
+    var cropper = this._cropper;
+    console.log("touchstart.");
+    var el = cropper.canvas;
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        console.log("touchstart:" + i + "...");
+        if (cropper.ongoingTouchIndexById(touches[i].identifier) === -1) {
+        	cropper.ongoingTouches.push(cropper.copyTouch(touches[i]));
+        	console.log("touchstart:" + i + ".");
+        }
+    }
 };
 
-Cropper.prototype.handleCancel = function (evt) {
-	var cropper = this._cropper;
-	evt.preventDefault();
-	console.log("touchcancel.");
-	var touches = evt.changedTouches;
-	
-	for (var i = 0; i < touches.length; i++) {
-	  var idx = ongoingTouchIndexById(touches[i].identifier);
-	  ongoingTouches.splice(idx, 1);  // remove it; we're done
-	}
+Cropper.prototype.handleMove = function(evt) {
+
+    evt.preventDefault();
+    var cropper = this._cropper;
+    var canvas = cropper.canvas;
+    var ctx = canvas.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var color = colorForTouch(touches[i]);
+        var idx = cropper.ongoingTouchIndexById(touches[i].identifier);
+        if (idx >= 0) {
+
+            var ongoingTouche = cropper.ongoingTouches[idx];
+            var touch = touches[i];
+            var deltaX = touch.pageX - ongoingTouche.pageX;
+            var deltaY = touch.pageY - ongoingTouche.pageY;
+
+            console.log('clearRect');
+            ctx.save();
+            console.log(cropper.originX - cropper.curOriginX, cropper.originY - cropper.curOriginY)
+            ctx.translate(cropper.originX - cropper.curOriginX, cropper.originY - cropper.curOriginY);
+            ctx.clearRect(0, 0, cropper.canvasWidth, cropper.canvasHeight);
+            ctx.restore();
+            console.log('clearRect done');
+
+            console.log('drawImage');
+            console.log(deltaX, deltaY);
+            console.log(cropper.originX + deltaX, cropper.originX + deltaY)
+            ctx.translate(cropper.originX + deltaX, cropper.originX + deltaY);
+            cropper.curOriginX = cropper.originX + deltaX;
+            cropper.curOriginY = cropper.originY + deltaY;
+            ctx.drawImage(
+            	cropper.img,
+            	0, 0,
+            	cropper.imgWidth, cropper.imgHeight,
+            	0, 0,
+            	cropper.imgWidth, cropper.imgHeight
+            );
+            console.log('drawImage done');
+            cropper.ongoingTouches.splice(idx, 1, cropper.copyTouch(touches[i])); // swap in the new touch record
+
+        } else {
+            console.log("can't figure out which touch to continue");
+        }
+    }
+};
+
+Cropper.prototype.touchend = function(evt) {
+
+    evt.preventDefault();
+    var cropper = this._cropper;
+    log("touchend");
+    var el = cropper.canvas;
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var color = colorForTouch(touches[i]);
+        var idx = cropper.ongoingTouchIndexById(touches[i].identifier);
+
+        if (idx >= 0) {
+            ctx.lineWidth = 4;
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(cropper.ongoingTouches[idx].pageX, cropper.ongoingTouches[idx].pageY);
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
+            cropper.ongoingTouches.splice(idx, 1); // remove it; we're done
+        } else {
+            console.log("can't figure out which touch to end");
+        }
+    }
+};
+
+Cropper.prototype.handleCancel = function(evt) {
+
+    evt.preventDefault();
+    var cropper = this._cropper;
+    console.log("touchcancel.");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var idx = cropper.ongoingTouchIndexById(touches[i].identifier);
+        cropper.ongoingTouches.splice(idx, 1); // remove it; we're done
+    }
 };
 
 
 
-Cropper.prototype.crop = function () {
-	
+Cropper.prototype.crop = function() {
+
 };
--- asserts/js/index.js
+++ asserts/js/index.js
@@ -8,32 +8,27 @@ var imgUrl = 'https://caijw.github.io/asserts/img/Lena.jpg';
 var imgUrl = './asserts/img/Lena.jpg';
 var imgWidth = 400;
 var imgHeight = 225;
+var croper = null;
+
 function init() {
-	return new Promise(function (resolve, reject) {
-		$origin.append(`<canvas id="canvas" width=300 height=300></canvas>`);
-		$canvas = $('#canvas');
-		var img = new Image(); 
-		img.onload = function(){
-			var ctx = $canvas[0].getContext('2d');
-			// ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-			resolve();
-		};
-		img.onerror = function(err){
-			reject(err);
-		};
-		img.src = imgUrl;
-	});
+	$origin.append(`<canvas id="canvas" width=300 height=300></canvas>`);
+	$canvas = $('#canvas');
 }
 
 function bind() {
-	
+	$cropBtn.on('click', function (evt) {
+		croper.crop();
+	});
 }
 var vConsole = new VConsole();
-init().then(function () {
-	var croper = new Cropper({
-		canvas: $canvas[0]
-	});
+init();
+croper = new Cropper({
+	canvas: $canvas[0],
+	imgUrl: imgUrl
+});
+croper.init().then(function (resolve, reject) {
+	bind();
 }).catch(function (err) {
-	alert('初始化失败');
 	console.error(err);
-})
+});
+
