d80b0124f7428f59022818c03d62a1e02718439d
kingweicai(蔡经纬) <kingweicai@tencent.com>
2019-01-02 11:07:02 +0800

    feat: debug

--- asserts/js/crop.js
+++ asserts/js/crop.js
@@ -226,7 +226,7 @@ Cropper.prototype.handleMove = function(evt) {
 			console.log('translateDeltaX:', translateDeltaX, 'translateDeltaY:', translateDeltaY);
 			ctx.clearRect(cropper.startX, cropper.startY, cropper.imgWidth, cropper.imgHeight);
 			/*让两个touch点的中点成为坐标原点*/
-			ctx.translate(translateDeltaX, translateDeltaY);
+			// ctx.translate(translateDeltaX, translateDeltaY);
 			ctx.scale(scale, scale);
 			// ctx.drawImage(
 			// 	cropper.img,
@@ -235,7 +235,7 @@ Cropper.prototype.handleMove = function(evt) {
 			// 	-curMidTouch.x, -curMidTouch.y,
 			// 	cropper.imgWidth, cropper.imgHeight
 			// );
-			ctx.translate(-translateDeltaX, -translateDeltaY);
+			// ctx.translate(-translateDeltaX, -translateDeltaY);
 			ctx.drawImage(
 				cropper.img,
 				0, 0,
